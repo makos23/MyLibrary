b9f99b040d097a86371bac619a68082431fdfb30
makos23 <lukasz.makowski1983@gmail.com>
2018-07-15 15:27:23 +0200

    First commit

---
+++ src/main/java/pl/moja/biblioteczka/controllers/MainController.java
@@ -0,0 +1,9 @@
+package pl.moja.biblioteczka.controllers;
+
+import javafx.event.ActionEvent;
+
+public class MainController {
+    public void click(ActionEvent actionEvent) {
+        System.out.println("Hello World");
+    }
+}
