f5463e9044d977620f1ef336e91db431d13cf36e
makos23 <lukasz.makowski1983@gmail.com>
2018-07-15 19:45:19 +0200

    Resource Bundles

--- src/main/java/pl/moja/biblioteczka/Main.java
+++ src/main/java/pl/moja/biblioteczka/Main.java
@@ -6,6 +6,9 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class Main extends Application {
 
     public static void main(String[] args) {
@@ -14,11 +17,14 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        Locale.setDefault(new Locale("en"));
         FXMLLoader loader= new FXMLLoader(this.getClass().getResource("/fxml/BorderPaneMain.fxml"));
+        ResourceBundle bundle= ResourceBundle.getBundle("bundles.messages");
+        loader.setResources(bundle);
         Parent root = loader.load();
         Scene scene= new Scene(root);
         primaryStage.setScene(scene);
-        primaryStage.setTitle("Moja Biblioteczka");
+        primaryStage.setTitle(bundle.getString("tittle.application"));
         primaryStage.show();
     }
 
